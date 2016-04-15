587fda097a3057be59f32f93dec1d6ad12de2f01
Guilherme <guijac@gmail.com>
2016-04-15 15:54:49 -0300

    Criacao da classe "main"

--- AlgoritmosGeneticos/src/br/usp/ia/main/ExecucaoGeneticos.java
+++ AlgoritmosGeneticos/src/br/usp/ia/main/ExecucaoGeneticos.java
@@ -2,6 +2,17 @@ package br.usp.ia.main;
 
 public class ExecucaoGeneticos {
 
+	/*
+	 * A execucao do programa espera os seguintes parametros de entrada:
+	 * 	
+	 *  1. Funcao a ser otimizada;
+	 * 	2. Porcentagens para execuc˜ao de parametros;
+	 *  3. Numero de geracoes
+	 *  4. Numero de cromossomos
+	 *  
+	 * Sendo assim uma chamada neste formato: java ExecucaoGeneticos A 50 10 50
+	 *  
+	 */
 	public static void main(String[] args) {
 		
 		
