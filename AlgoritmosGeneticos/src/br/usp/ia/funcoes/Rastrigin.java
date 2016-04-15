67c78818989e8ebc4fdf1080d8ed0fa38e35b7a8
Guilherme <guijac@gmail.com>
2016-04-15 16:43:44 -0300

    Preparo das classes

---
+++ AlgoritmosGeneticos/src/br/usp/ia/funcoes/Rastrigin.java
@@ -0,0 +1,7 @@
+package br.usp.ia.funcoes;
+
+import br.usp.ia.interfaces.FuncaoFitness;
+
+public class Rastrigin implements FuncaoFitness {
+
+}
---
+++ AlgoritmosGeneticos/src/br/usp/ia/interfaces/FuncaoFitness.java
@@ -0,0 +1,7 @@
+package br.usp.ia.interfaces;
+
+public interface FuncaoFitness {
+
+	
+	
+}
